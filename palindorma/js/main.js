84021e0aa06d557f16fdfca20c5c587d71cde580
CaldatoLuca <lucacaldato15@gmail.com>
2023-11-14 16:27:49 +0100

    feat: creazione della condizione e della function

--- palindorma/js/main.js
+++ palindorma/js/main.js
@@ -12,6 +12,37 @@
 // !FUNZIONI
 //  ?-------------
 
+function palindroma(parola) {
+  const word = [];
+  const reverseWord = [];
+  let resoult = false;
+
+  for (let i = 0; i < userInput.length; i++) {
+    word.push(userInput[i]);
+  }
+  for (let i = userInput.length - 1; i >= 0; i--) {
+    reverseWord.push(userInput[i]);
+  }
+
+  console.log(word);
+  console.log(reverseWord);
+
+  for (let i = 0; i < userInput.length; i++) {
+    if (word[i] === reverseWord[i]) {
+      resoult = true;
+    } else {
+      resoult = false;
+      break;
+    }
+  }
+  if (resoult === true) {
+    console.log("La parola è palindroma");
+  } else if (resoult === false) {
+    console.log("La parola non è palindroma");
+  }
+  return resoult;
+}
+
 //  ?-------------
 // !CODICE
 //  ?-------------
@@ -19,12 +50,5 @@
 const userInput = prompt(
   "Inserisci una parola o una frase per scoprire se è palindroma"
 );
-const word = [];
-const reverseWord = [];
 
-for (let i = 0; i < userInput.length; i++) {
-  word.push(userInput[i]);
-}
-for (let i = userInput.length - 1; i >= 0; i--) {
-  reverseWord.push(userInput[i]);
-}
+palindroma(userInput);
