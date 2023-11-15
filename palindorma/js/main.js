66ef6ab373bc867e4d809ec2a28af0964f011fa1
CaldatoLuca <lucacaldato15@gmail.com>
2023-11-15 11:56:07 +0100

    correzzione

--- palindorma/js/main.js
+++ palindorma/js/main.js
@@ -1,3 +1,12 @@
+// 1. Fare prompt per richiedere la parola all' utente
+// 2. Creare una funzione che avrà come risultato true o false in base alla parola inserita (NB ricorda return)
+// 3. Risolzione parola palindroma
+//    - parola letta al contrario non cambia
+//    - trattare la stringa come array
+//    - se la lettera in posizione i è uguale sia leggendola da sx a dx che viceversa è palindroma
+//    - confronto con ciclo le varie lettere nelle due letture (se corrispondo è palindroma)
+//    - non dovrebbe essere necessario finire la lettura a metà parola (se si riesce meglio, evito calcoli inutili)
+
 "use strict";
 //  ?-------------
 // !FUNZIONI
@@ -59,4 +68,40 @@ const userInput = prompt(
 //*richiamo la funzion e la uso con userInput
 palindroma(userInput);
 
-//!TODO non funziona con !,? e caratteri speciali, sostituire ciclo for col while
+/* //!correzzione 
+?uso ciclo while e non creo due array appesantendo il codice, la condizione che da il riuslato sta fuori la funzione ma la usa per ridare gli alert
+//  ?-------------
+ !FUNZIONI
+//  ?-------------
+function palindroma(parola) {
+if (parola.length === 0) return false;
+if (parola.length === 1) return true;
+
+const parolaMinuscola = parola.toLowerCase();
+let inizio = 0;
+let fine = parolaMinuscola.length - 1;
+
+while(inizio < fine){
+  if(parolaMinuscola[inizio] !== parolaMinuscola[fine]) return false;
+  inizio++;
+  fine--;
+}
+
+return true;
+
+}
+
+//  ?-------------
+ !CODICE
+//  ?-------------
+const userInput = prompt(
+  "Inserisci una parola per scoprire se è palindroma"
+);
+
+  if (palindroma(userInput)) {
+    console.log("La parola o frase è palindroma");
+  } else{
+       console.log("La parola o frase non è palindroma");
+  } 
+ 
+*/
